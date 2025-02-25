7459f9d3422fedd6e3239e8e352c6b3e5cc7dcd6
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-02-25 23:31:54 +0100

    remove wrong backslashes in html tags for leafnodes

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -9,7 +9,7 @@ class TestLeafNode(unittest.TestCase):
         node3 = LeafNode("p", "This is cool")
         node4 = LeafNode("body", None)
         self.assertEqual(repr(node), "HTMLNode(h1, miku, None, None)")
-        self.assertEqual(node.to_html(), "<h1>miku<\h1>")
+        self.assertEqual(node.to_html(), "<h1>miku</h1>")
         self.assertEqual(node.props_to_html(), None)
 
 if __name__ == "__main__":
