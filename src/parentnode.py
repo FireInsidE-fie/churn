489530ebe190e97dfcc59da881c3ae8607ed01d0
Aisling <aisling.fontaine@protonmail.com>
2025-02-26 21:12:19 +0100

    begin adding parentnode

---
+++ src/parentnode.py
@@ -0,0 +1,14 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        self.tag = tag
+        self.children = children
+        self.props = property
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("ParentNode must have a tag")
+        if not self.children:
+            raise ValueError("ParentNode must have children")
+        return f"<{self.tag}>{self.value}</{self.tag}>"
