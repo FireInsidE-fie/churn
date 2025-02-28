3906233e7485e0ff8ff212686eb8552e85f01903
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-02-28 12:50:25 +0100

    add first parentnode test

---
+++ src/test_parentnode.py
@@ -0,0 +1,18 @@
+import unittest
+
+from leafnode import LeafNode
+from parentnode import ParentNode
+
+class TestParentNode(unittest.TestCase):
+    def test_basic_parentnode(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+
+        self.assertEqual(node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
