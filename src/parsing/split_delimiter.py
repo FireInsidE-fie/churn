f16289b0e94afd0e298b366bb7943c5c7b91faea
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-03-04 19:18:44 +0100

    finish split_nodes_delimiter and add more tests

--- src/parsing/split_delimiter.py
+++ src/parsing/split_delimiter.py
@@ -1,13 +1,14 @@
 from src.textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    if not old_nodes or not delimiter or not text_type:
+        raise ValueError("split_nodes_delimiter needs valid values")
     new_nodes = []
     for node in old_nodes:
         slices = node.text.split(delimiter)
         new_nodes.append(TextNode(slices[0], TextType.NORMAL))
         new_nodes.append(TextNode(slices[1], text_type))
         new_nodes.append(TextNode(slices[2], TextType.NORMAL))
-    print(new_nodes)
     return new_nodes
 
 
--- src/parsing/test_split_delimiter.py
+++ src/parsing/test_split_delimiter.py
@@ -13,3 +13,42 @@ class TestSplitDelimiter(unittest.TestCase):
             TextNode("code block", TextType.CODE),
             TextNode(" word", TextType.NORMAL)
         ])
+
+    def test_split_delimiter2(self):
+        node = TextNode("This is text with a *strong* word", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.BOLD)
+
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.NORMAL),
+            TextNode("strong", TextType.BOLD),
+            TextNode(" word", TextType.NORMAL)
+        ])
+
+    def test_split_delimiter3(self):
+        node = TextNode("This is text with *a `strong` word*", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.BOLD)
+
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with ", TextType.NORMAL),
+            TextNode("a `strong` word", TextType.BOLD),
+            TextNode("", TextType.NORMAL)
+        ])
+
+    def test_split_delimiter4(self):
+        node = TextNode("This is text with a /italic/ word", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([node], "/", TextType.ITALIC)
+
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.NORMAL),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word", TextType.NORMAL)
+        ])
+
+    def test_split_delimiter_empty(self):
+        node = TextNode("This is text with *a `strong` word*", TextType.NORMAL)
+        with self.assertRaises(AttributeError):
+            split_nodes_delimiter(node, "*", TextType.WOW)
+        with self.assertRaises(ValueError):
+            new_nodes1 = split_nodes_delimiter(None, "*", TextType.BOLD)
+            new_nodes2 = split_nodes_delimiter(node, "", TextType.BOLD)
+            new_nodes3 = split_nodes_delimiter(node, "*", None)
