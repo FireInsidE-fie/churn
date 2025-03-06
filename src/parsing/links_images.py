f80f6ffd92d52aee84a659c33fb31076ffc49cca
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-03-06 22:18:17 +0100

    add extract_markdown_links and test

--- src/parsing/links_images.py
+++ src/parsing/links_images.py
@@ -3,8 +3,21 @@ import re
 def extract_markdown_images(text):
     parsed_data = []
     images = re.findall(r"!\[[^\[\]]+]?\([^()]+\)", text)
+    if not images:
+        return None
     for image in images:
-        alt_text = re.search(r"!\[[^\[\]]+]", image)
-        url = re.search(r"\([^()]+\)", image)
+        alt_text = re.findall(r"!\[[^\[\]]+]", image)[0][2:-1]
+        url = re.findall(r"\([^()]+\)", image)[0][1:-1]
+        parsed_data.append((alt_text, url))
+    return parsed_data
+
+def extract_markdown_links(text):
+    parsed_data = []
+    links = re.findall(r"\[[^\[\]]+]?\([^()]+\)", text)
+    if not links:
+        return None
+    for link in links:
+        alt_text = re.findall(r"\[[^\[\]]+]", link)[0][1:-1]
+        url = re.findall(r"\([^()]+\)", link)[0][1:-1]
         parsed_data.append((alt_text, url))
     return parsed_data
--- src/parsing/test_links_images.py
+++ src/parsing/test_links_images.py
@@ -1,8 +1,49 @@
 import unittest
 
-from src.textnode import TextNode, TextType
-from links_images import extract_markdown_images
+from links_images import extract_markdown_images, extract_markdown_links
 
 class TestMarkdownImages(unittest.TestCase):
     def test_markdown_images1(self):
-        pass
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) wowowowowo"
+        images = extract_markdown_images(text)
+        self.assertEqual(images[0], ("rick roll", "https://i.imgur.com/aKaOqIh.gif"))
+        self.assertEqual(images[1], ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"))
+
+    def test_markdown_images2(self):
+        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        images = extract_markdown_images(text)
+        self.assertEqual(images[0], ("image", "https://i.imgur.com/zjjcJKZ.png"))
+
+    def test_markdown_images3(self):
+        text = "This is text with no image ![]()"
+        images = extract_markdown_links(text)
+        self.assertEqual(images, None)
+
+    def test_markdown_images4(self):
+        text = "two ![images](wow.html)?? this is ![crazy!!!](wow2.html)"
+        images = extract_markdown_links(text)
+        self.assertEqual(images[0], ("images", "wow.html"))
+        self.assertEqual(images[1], ("crazy!!!", "wow2.html"))
+
+class TestMarkdownLinks(unittest.TestCase):
+    def test_markdown_links1(self):
+        text = "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg) wowowowowo"
+        links = extract_markdown_links(text)
+        self.assertEqual(links[0], ("rick roll", "https://i.imgur.com/aKaOqIh.gif"))
+        self.assertEqual(links[1], ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"))
+
+    def test_markdown_links2(self):
+        text = "This is text with a [link](https://i.imgur.com/zjjcJKZ.png)"
+        links = extract_markdown_links(text)
+        self.assertEqual(links[0], ("link", "https://i.imgur.com/zjjcJKZ.png"))
+
+    def test_markdown_links3(self):
+        text = "This is text with no link ![]()"
+        links = extract_markdown_links(text)
+        self.assertEqual(links, None)
+
+    def test_markdown_links4(self):
+        text = "two [links](wow.html)?? this is [crazy!!!](wow2.html)"
+        links = extract_markdown_links(text)
+        self.assertEqual(links[0], ("links", "wow.html"))
+        self.assertEqual(links[1], ("crazy!!!", "wow2.html"))
