517559a2c9b2df8f9653deab19a860d582cf303e
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-03-17 17:57:33 +0100

    begin block_to_block_type

--- src/parsing/blocks.py
+++ src/parsing/blocks.py
@@ -1,3 +1,41 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph",
+    HEADING = "heading",
+    CODE = "code",
+    QUOTE = "quote",
+    UNORDERED_LIST = "unordered_list",
+    ORDERED_LIST = "ordered_list",
+
+def is_heading(block_str):
+    pass
+
+def is_code(block_str):
+    pass
+
+def is_quote(block_str):
+    pass
+
+def is_unordered_list(block_str):
+    pass
+
+def is_ordered_list(block_str):
+    pass
+
+def block_to_block_type(block_str):
+    if is_heading(block_str):
+        return BlockType.HEADING
+    if is_code(block_str):
+        return BlockType.CODE
+    if is_quote(block_str):
+        return BlockType.QUOTE
+    if is_unordered_list(block_str):
+        return BlockType.UNORDERED_LIST
+    if is_ordered_list(block_str):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
